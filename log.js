2fa83b796d69b1cb349b816e4d32978f099972aa
Malakai Shaylin C <66368978+Shaylin-8bit@users.noreply.github.com>
2022-02-15 03:32:07 +0000

    Finished boot log

--- log.js
+++ log.js
@@ -2,5 +2,5 @@ import { getLogs } from './utility/getLogs.js';
 
 export const log = async (ctx, client, type) => {
   const logs = await getLogs();
-  logs[type](ctx, client, type);
+  logs[type](ctx, client);
 }
--- logs/boot.js
+++ logs/boot.js
@@ -1,5 +1,12 @@
-const boot = () => {
-  console.log('boot');
+const boot = (ctx, client) => {
+  const channelName = 'bot-logs';
+  client.channels.cache.forEach(
+    (channel) => {
+      if (channel.name === channelName) {
+        channel.send('Bot restarted...');
+      }
+    }
+  );
 }
 
 export { boot };
